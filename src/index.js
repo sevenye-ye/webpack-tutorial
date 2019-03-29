31ca5789503a1a3855e9c7fb98fe9e44022f9e53
sevenye-ye <ylinylin777.gmail.com>
2019-03-30 00:17:22 +0100

    loading image

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
-  let element = document.createElement('div');
+	let element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+	// Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+  
+	// Add the image to our existing div.
+	var myIcon = new Image();
+	myIcon.src = Icon;
 
-  return element;
+	element.appendChild(myIcon);
+
+	return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module:{
-	  rules:[{test:/\.css$/,use:['style-loader','css-loader']}]
-  }
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module:{
+		rules:[
+			{test:/\.css$/,use:['style-loader','css-loader']},
+			{test:/\.(png|svg|jpg|gif)$/,use:['file-loader']}	  
+		]  
+	}
 };
