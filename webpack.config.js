60c64e0b220f7642744afae22da9960cf137990d
sevenye-ye <ylinylin777.gmail.com>
2019-03-30 00:29:13 +0100

    loading fonts

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
 	module:{
 		rules:[
 			{test:/\.css$/,use:['style-loader','css-loader']},
-			{test:/\.(png|svg|jpg|gif)$/,use:['file-loader']}	  
+			{test:/\.(png|svg|jpg|gif)$/,use:['file-loader']},
+			{test:/\.(woff|woff2|eot|ttf|otf)$/,use:['file-loader']}
 		]  
 	}
 };
